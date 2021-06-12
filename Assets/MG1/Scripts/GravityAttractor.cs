8b49770aa34435f0c0af0a08cf66e651232f99a3
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2021-06-12 16:23:32 +0000

    No Comments

---
+++ Assets/MG1/Scripts/GravityAttractor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityAttractor : MonoBehaviour
+{
+    public float Gravity = 9.8f;
+    
+
+    public void Attract(Transform body)
+    {
+        Vector3 targetDir = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        body.rotation = Quaternion.FromToRotation(bodyUp, targetDir) * body.rotation;
+        body.GetComponent<Rigidbody>().AddForce(targetDir * Gravity);
+    }
+}
