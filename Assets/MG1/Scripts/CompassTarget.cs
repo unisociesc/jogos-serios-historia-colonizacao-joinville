05ab49b9333055139020c7a0808a6e30da9a3605
Leonardo GB <leonardog.bat@gmail.com>
2021-06-23 19:39:21 +0000

    Adicionando compasso que aponta em direção a um objeto (ShipWorld)

---
+++ Assets/MG1/Scripts/CompassTarget.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassTarget : MonoBehaviour {
+    
+    RectTransform rt;
+    public Transform target; 
+
+    void Start() {
+        rt = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Pega a posicao do objeto no espaco da tela
+        Vector3 objScreenPos = Camera.main.WorldToScreenPoint(target.transform.position);
+
+        // Pega a direcao entre o compasso e a TargetArea
+        Vector3 dir = (objScreenPos - rt.position).normalized;
+        float angle = Mathf.Rad2Deg * Mathf.Atan2(dir.z, dir.x);
+        rt.rotation = Quaternion.AngleAxis(angle - 140, Vector3.forward);
+    }
+}
